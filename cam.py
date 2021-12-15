84b2dc6d9cce37d98b9a0d39afab78958dda5bb1
shahirg <43625705+shahirg@users.noreply.github.com>
2021-12-15 01:11:42 -0500

    reformat

--- cam.py
+++ cam.py
@@ -3,10 +3,13 @@ from time import sleep
 
 def take_picture(file_path, rotation=True, wait_time=5):
     camera = PiCamera()
-    camera.start_preview()
     if(rotation):
         camera.rotation = 180
+        
+    camera.start_preview()
     sleep(wait_time)
+
     camera.capture(file_path)
+
     camera.stop_preview()
     camera.close()
