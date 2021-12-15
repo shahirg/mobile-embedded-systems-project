b2a41675df8180f7beab8d9c99e342cbe0de7608
shahirg <43625705+shahirg@users.noreply.github.com>
2021-12-15 01:12:06 -0500

    update locking_cli

--- create_lock_cli.py
+++ create_lock_cli.py
@@ -1,79 +1,68 @@
-from picamera import PiCamera
+from cam import take_picture
 from time import sleep
-import face_recognition
+from encryption import generate_key, encrypt
+import face_recognition as fr
 import numpy as np
-# file to lockf
-file = input("Enter file path of file you want to lock:")
+
+PROJECT_DIR = '/home/pi/Documents/test/'
+# file to lock
+file_encrypt = input("Enter file path of file you want to encrypt:")
 
 #images
 upload = input('Would you like to upload images or take them now: (y/n)')
-#upload
+
 
 if upload == 'y':
     upload_path = '/home/pi/Documents/test/sg1.jpg'
     #upload_path = input("Enter file path of image 1:")
-    known_image = face_recognition.load_image_file(upload_path)
+    known_image = fr.load_image_file(upload_path)
 else:
-
     sleep(1)
-    cam = PiCamera()
-    cam.start_preview()
-    #cam.rotation = 180
-    sleep(3)
-    cam.capture(f'/home/pi/Documents/test/known.jpg')
-    known_image = face_recognition.load_image_file('/home/pi/Documents/test/known.jpg')
+    take_picture(file_path=f'{PROJECT_DIR}known.jpg')
+    known_image = fr.load_image_file(f'{PROJECT_DIR}known.jpg')
 
 try:
-    known_encoding = face_recognition.face_encodings(known_image)[0]
+    known_encoding = fr.face_encodings(known_image)[0]
     print(repr(known_encoding))
 except(IndexError):
     print('Face not detected')
     exit()
 
 
-with open('/home/pi/Documents/test/encoding.txt', 'w') as f:
+with open(f'{PROJECT_DIR}encoding.txt', 'w') as f:
    f.write(np.array_repr(known_encoding))
+
+key = generate_key(file_path=f'{PROJECT_DIR}mykey.key')
+encrypt(key=key,file_path=file_encrypt)
+
 
 #np.savetxt('/home/pi/Documents/test/encoding2.txt', known_image.reshape(known_image.shape[0],-1))
 
-sleep(5)
+# sleep(5)
 
-from DstTst import check_dist
-from gpiozero import MotionSensor
+# from distance import get_distance
+# from gpiozero import MotionSensor
 
-pir = MotionSensor(4)
-cam = PiCamera()
+# pir = MotionSensor(4)
 
-while(True):
-    pir.wait_for_motion()
-    print("Motion detected")
-    sleep(1)
-    check_dist()
-    sleep(1)
-    cam.start_preview()
-    #cam.rotation = 180
+# while(True):
+#     pir.wait_for_motion()
+#     print("Motion detected")
+#     sleep(1)
+#     get_distance()
+#     sleep(1)
+#     take_picture(file_path='/home/pi/Documents/test/unknown.jpg')
+#     break
 
-    sleep(5)
-    cam.capture(f'/home/pi/Documents/test/unknown.jpg')
-    cam.stop_preview()
-    cam.close()
-
-
-    break
-    sleep(2)
-    #pir.wait_for_no_motion()
-
-    print("Motion stopped")
-
-unknown_image = face_recognition.load_image_file('/home/pi/Documents/test/unknown.jpg')
-try:
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-except(IndexError):
-    print('No Face Detected')
-    exit()
-results = face_recognition.compare_faces([known_encoding], unknown_encoding)
-print(results)
-if(results[0] == True):
-    print('unlocked')
-else:
-    print('still locked')
+# unknown_image = face_recognition.load_image_file('/home/pi/Documents/test/unknown.jpg')
+# try:
+#     unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+# except(IndexError):
+#     print('No Face Detected')
+#     exit()
+# results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+# print(results)
+# if(results[0] == True):
+#     print('unlocked')
+# else:
+#     print('still locked')
